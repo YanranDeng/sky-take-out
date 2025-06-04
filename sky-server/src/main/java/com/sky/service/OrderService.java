8fdf249bd131fb0089ff116d69e20de073e4f612
dengyanran <2018594047@qq.com>
2025-06-04 16:16:27 +0800

    订单支付功能代码

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -9,5 +9,22 @@ import com.sky.vo.OrderVO;
 
 public interface OrderService {
 
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
     OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+    /**
+     * 订单支付
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
 }
