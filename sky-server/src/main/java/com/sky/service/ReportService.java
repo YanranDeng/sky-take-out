7f2831b95a4fb52efb38913dfab21e76f2aa1145
dengyanran <2018594047@qq.com>
2025-06-07 10:47:45 +0800

    营业额统计代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -4,47 +4,13 @@ import com.sky.vo.OrderReportVO;
 import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
 
 public interface ReportService {
-    /**
-     * 营业额统计
-     * @param begin
-     * @param end
-     * @return
-     */
-    TurnoverReportVO turnoverStatistics(LocalDate begin, LocalDate end);
 
-    /**
-     * 用户数量统计
-     * @param begin
-     * @param end
-     * @return
-     */
-    UserReportVO userStatistics(LocalDate begin, LocalDate end);
-
-    /**
-     * 订单统计
-     * @param begin
-     * @param end
-     * @return
-     */
-    OrderReportVO getOrdersStatistics(LocalDate begin, LocalDate end);
-
-    /**
-     * 统计销量排名前10的菜品或套餐
-     * @param begin
-     * @param end
-     * @return
-     */
-    SalesTop10ReportVO getTop10(LocalDate begin, LocalDate end);
-
-    /**
-     * 导出excel报表数据
-     * @param response
-     */
-    void exportExcel(HttpServletResponse response) throws FileNotFoundException;
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sky.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -15,6 +16,7 @@ import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.*;
 import com.sky.result.PageResult;
 import com.sky.service.OrderService;
+import com.sky.utils.HttpClientUtil;
 import com.sky.utils.WeChatPayUtil;
 import com.sky.vo.DishVO;
 import com.sky.vo.OrderPaymentVO;
@@ -24,6 +26,7 @@ import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
@@ -38,8 +41,16 @@ import java.util.Map;
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
+
+    @Value("${sky.shop.address}")
+    private String shopAddress;
+
+    @Value("${sky.baidu.ak}")
+    private String ak;
+
     @Autowired
     private WebSocketServer webSocketServer;
+
     @Autowired
     private OrderMapper orderMapper;
 
@@ -57,6 +68,70 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private WeChatPayUtil weChatPayUtil;
+
+    /**
+     * 检查客户的收货地址是否超出配送范围
+     * @param address
+     */
+    private void checkOutOfRange(String address) {
+        Map map = new HashMap();
+        map.put("address",shopAddress);
+        map.put("output","json");
+        map.put("ak",ak);
+
+        //获取店铺的经纬度坐标
+        String shopCoordinate = HttpClientUtil.doGet("https://api.map.baidu.com/geocoding/v3", map);
+
+        JSONObject jsonObject = JSON.parseObject(shopCoordinate);
+        if(!jsonObject.getString("status").equals("0")){
+            throw new OrderBusinessException("店铺地址解析失败");
+        }
+
+        //数据解析
+        JSONObject location = jsonObject.getJSONObject("result").getJSONObject("location");
+        String lat = location.getString("lat");
+        String lng = location.getString("lng");
+        //店铺经纬度坐标
+        String shopLngLat = lat + "," + lng;
+
+        map.put("address",address);
+        //获取用户收货地址的经纬度坐标
+        String userCoordinate = HttpClientUtil.doGet("https://api.map.baidu.com/geocoding/v3", map);
+
+        jsonObject = JSON.parseObject(userCoordinate);
+        if(!jsonObject.getString("status").equals("0")){
+            throw new OrderBusinessException("收货地址解析失败");
+        }
+
+        //数据解析
+        location = jsonObject.getJSONObject("result").getJSONObject("location");
+        lat = location.getString("lat");
+        lng = location.getString("lng");
+        //用户收货地址经纬度坐标
+        String userLngLat = lat + "," + lng;
+
+        map.put("origin",shopLngLat);
+        map.put("destination",userLngLat);
+        map.put("steps_info","0");
+
+        //路线规划
+        String json = HttpClientUtil.doGet("https://api.map.baidu.com/directionlite/v1/driving", map);
+
+        jsonObject = JSON.parseObject(json);
+        if(!jsonObject.getString("status").equals("0")){
+            throw new OrderBusinessException("配送路线规划失败");
+        }
+
+        //数据解析
+        JSONObject result = jsonObject.getJSONObject("result");
+        JSONArray jsonArray = (JSONArray) result.get("routes");
+        Integer distance = (Integer) ((JSONObject) jsonArray.get(0)).get("distance");
+
+        if(distance > 20000){
+            //配送距离超过5000米
+            throw new OrderBusinessException("超出配送范围");
+        }
+    }
 
     /**
      * 用户下单
@@ -80,6 +155,8 @@ public class OrderServiceImpl implements OrderService {
         if(shoppingCartList == null || shoppingCartList.size() == 0){
             throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
         }
+        //1.3 检查用户的收获地址是否超出配送范围
+        checkOutOfRange(addressBook.getCityName()+addressBook.getDistrictName()+addressBook.getDetail());
 
         //2. 向订单表插入1条数据
         Orders orders = new Orders();
@@ -253,6 +330,11 @@ public class OrderServiceImpl implements OrderService {
 
     }
 
+    /**
+     * 获取商品详细信息
+     * @param id
+     * @return
+     */
     @Override
     public OrderVO getDetails(Long id) {
         Orders orders = orderMapper.getById(id);
@@ -266,6 +348,10 @@ public class OrderServiceImpl implements OrderService {
         return orderVO;
     }
 
+    /**
+     * 已接单
+     * @param ordersConfirmDTO
+     */
     @Override
     public void confirm(OrdersConfirmDTO ordersConfirmDTO) {
         Orders orders = orderMapper.getById(ordersConfirmDTO.getId());
@@ -277,6 +363,10 @@ public class OrderServiceImpl implements OrderService {
         orderMapper.update(orders);
     }
 
+    /**
+     * 完成
+     * @param id
+     */
     @Override
     public void complete(Long id) {
         Orders orders = orderMapper.getById(id);
@@ -287,6 +377,11 @@ public class OrderServiceImpl implements OrderService {
         orders.setPayStatus(Orders.COMPLETED);
         orderMapper.update(orders);
     }
+
+    /**
+     * 取消订单
+     * @param ordersCancelDTO
+     */
 
     @Override
     public void cancel(OrdersCancelDTO ordersCancelDTO) {
@@ -302,6 +397,11 @@ public class OrderServiceImpl implements OrderService {
         orderMapper.update(orders);
     }
 
+    /**
+     * 拒单
+     * @param ordersRejectionDTO
+     */
+
     @Override
     public void reject(OrdersRejectionDTO ordersRejectionDTO) {
         Orders orders = orderMapper.getById(ordersRejectionDTO.getId());
@@ -314,6 +414,11 @@ public class OrderServiceImpl implements OrderService {
         orders.setAmount(new BigDecimal("0"));
         orderMapper.update(orders);
     }
+
+    /**
+     * 再来一单
+     * @param id
+     */
 
     @Override
     public void repete(Long id) {
